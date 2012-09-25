41ec9182554b634efac57b678be6f6a94ff565f8
mrapple <tony@oc.tc>
2012-09-25 17:50:02 -0500

    Fix capistrano deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,38 @@
-set :application, "acssj"
-set :repository,  "git@github.com:mrapple/acssj.org.git"
+require "rvm/capistrano"
+require "bundler/capistrano"
 
-set :deploy_to, "/var/www/#{application}"
-set :deploy_via, :export
+set :application, "acssj"
+
+set :rvm_ruby_string, "ruby-1.9.2-p290"
+set :rvm_type, :user
+set :rvm_path, "$HOME/.rvm"
+set :rvm_bin_path, "$HOME/.rvm/bin"
+
+set :repository,  "git@github.com:mrapple/acssj.org.git"
 set :scm, :git
 set :branch, 'master'
-set :deploy_to, "/home/deploy/apps/acssj.org"
+
+set :deploy_to, "/home/deploy/apps/acssj"
+set :deploy_via, :export
 set :user, 'deploy'
 set :shared_children, []
 
 server 'zeus.oc.tc', :app, :web, :db, :primary => true
 
 namespace :deploy do
-  task :update do
-    transaction do
-      update_code
-      build_code
-      symlink
-    end
-  end
+  task :binstubs, :roles => :app, :except => { :no_release => true } do
+    run "cd #{current_path} && bundle install --binstubs"
+  end
 
-  task :build_code, :except => { :no_release => true } do
-    run "staticmatic build #{latest_release}"
-  end
+  task :update do
+    transaction do
+      update_code
+      build_code
+      create_symlink
+    end
+  end
+
+  task :build_code, :except => { :no_release => true } do
+    run "staticmatic build #{latest_release}"
+  end
 end
